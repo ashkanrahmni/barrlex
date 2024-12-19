49b87d099270681a0e574920292365cc538f9f5b
ashkan <74952588+ashkanrahmni@users.noreply.github.com>
2024-12-19 04:57:04 -0500

    Screen Window and SFML Test

---
+++ barrlex.cpp
@@ -0,0 +1,56 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1200, 600), "Barrlex Worm Die or alive");
+
+    std::string symbols = "%#@";
+    int symbolSize = 30;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Red);
+
+
+        for (int i = 0; i < window.getSize().x / symbolSize; ++i) {
+            sf::RectangleShape symbol(sf::Vector2f(symbolSize, symbolSize));
+            symbol.setFillColor(sf::Color::White);
+            symbol.setPosition(i * symbolSize, 0); /
+
+            window.draw(symbol);
+        }
+
+        for (int i = 0; i < window.getSize().x / symbolSize; ++i) {
+            sf::RectangleShape symbol(sf::Vector2f(symbolSize, symbolSize));
+            symbol.setFillColor(sf::Color::White);
+            symbol.setPosition(i * symbolSize, window.getSize().y - symbolSize); // پایین صفحه
+
+            window.draw(symbol);
+        }
+
+        for (int i = 0; i < window.getSize().y / symbolSize; ++i) {
+            sf::RectangleShape symbol(sf::Vector2f(symbolSize, symbolSize));
+            symbol.setFillColor(sf::Color::White);
+            symbol.setPosition(0, i * symbolSize); // سمت چپ صفحه
+
+            window.draw(symbol);
+        }
+
+        for (int i = 0; i < window.getSize().y / symbolSize; ++i) {
+            sf::RectangleShape symbol(sf::Vector2f(symbolSize, symbolSize));
+            symbol.setFillColor(sf::Color::White);
+            symbol.setPosition(window.getSize().x - symbolSize, i * symbolSize); // سمت راست صفحه
+
+            window.draw(symbol);
+        }
+
+        window.display();
+    }
+
+    return 0;
+}
